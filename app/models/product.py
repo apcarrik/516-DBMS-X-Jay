fbf6d48d1cc48dff610aae57f99b29862cf60442
Yue Li <yl819@duke.edu>
2022-04-02 19:43:23 -0400

    adding to cart function& cart basic display and total price

--- app/models/product.py
+++ app/models/product.py
@@ -77,3 +77,29 @@ WHERE inventory.pid = :id AND inventory.sid = users.id
 ''',
                               id=pid)
         return rows if rows is not None else None
+        
+    @staticmethod
+    def addToCart(uid,pid,sid,quantity):
+        existQt = app.db.execute('''
+SELECT quantity FROM Cart
+WHERE uid=:uid AND pid=:pid AND sid=:sid
+''',
+                uid=uid, pid=pid, sid=sid)
+        if len(existQt) > 0:
+            newQt = existQt[0][0] + quantity
+            rows = app.db.execute('''
+    UPDATE Cart
+    SET quantity = :newQt
+    WHERE uid=:uid AND pid=:pid AND sid=:sid
+    RETURNING pid
+    ''',
+                newQt=newQt, uid=uid, pid=pid, sid=sid)
+            return rows[0] if rows is not None else None
+            
+        else:
+            rows = app.db.execute('''
+    INSERT INTO Cart(uid, pid, sid, quantity)
+    VALUES(:uid, :pid, :sid, :quantity)
+    RETURNING pid
+    ''',            uid=uid, pid=pid, sid=sid,quantity=quantity)
+            return rows[0] if rows is not None else None
